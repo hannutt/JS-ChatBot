de73da03d04fd2e9f44cd4b39bfa8f7b2094c81e
hannutt <htuomela@gmail.com>
2024-04-25 03:28:56 +0300

    First version

---
+++ script.js
@@ -0,0 +1,65 @@
+const sendChatBtn = document.querySelector(".chat-input span")
+const chatInput = document.querySelector(".chat-input textarea")
+const chatbox = document.querySelector(".chatbox")
+let usermessage;
+let API_KEY=""
+var asked = 0
+document.getElementById("qty").innerHTML= asked
+
+const createChatLi = (message,className) => {
+    //luodaan <li> elementti jonka sisällä on message muuttujan sisältö
+    const chatLi = document.createElement("li")
+    chatLi.classList.add("chat",className)
+    //? : notaatio on ternary operaattori eli käytännössä if/else
+    let chatContent= className ==="outgoing" ? `<p>${message}</p>`: `<span class="material-symbols-outlined">Sentiment_Satisfied</span> <p>${message}</p>`
+    chatLi.innerHTML=chatContent
+    return chatLi
+    
+}
+
+
+const generateResponse = () => {
+    asked = asked+1
+    document.getElementById("qty").innerHTML=asked
+    const API_URL="https://api.openai.com/v1/chat/completions"
+    
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json",
+            "Authorization":`Bearer ${API_KEY}`
+        },
+        body:JSON.stringify({
+            model:"gpt-3.5-turbo",
+            messages:[{role: "user", content: usermessage}]
+        })
+
+
+    }
+    fetch(API_URL,requestOptions).then(res=>res.json()).then(data => {
+        console.log(data)
+        chatbox.appendChild(createChatLi(data.error.code,"incoming"))
+       
+    }).catch((error)=>{
+        console.log(error)
+        
+    })
+}
+const handleChat = () =>{
+    //textarea sisällön tallennus muuttujaan ja välilyöntien poistos
+    usermessage=chatInput.value.trim()
+    console.log(usermessage)
+    if (!usermessage) return;
+//käyttäjän viestin näyttö chatbox elementissä
+chatbox.appendChild(createChatLi(usermessage, "outgoing"))
+
+setTimeout(()=>{
+    //thinking teksti sijoitetaan incoming elementtiin
+    chatbox.appendChild(createChatLi("Thinking","incoming"))
+    generateResponse()
+},600)
+
+}
+
+sendChatBtn.addEventListener("click",handleChat)
